59625597b2cd0fa007057f94142c336dd02d0fc7
Bartosz Kaczmarczyk <bartosz.kaczmarczyk.pl@gmail.com>
2024-03-17 15:48:59 +0100

    fd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,41 @@
 package org.example;
 
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
+import org.example.graphics.Mesh;
+import org.example.graphics.Renderer;
+import org.example.graphics.Vertex;
+import org.example.math.Vector3f;
 
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.ARBVertexArrayObject.glBindVertexArray;
 import static org.lwjgl.opengl.ARBVertexArrayObject.glGenVertexArrays;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main {
-public void run(){
+
+    public Mesh mesh = new Mesh(new Vertex[] {
+            new Vertex(new Vector3f(-0.5f,  0.5f, 0.0f)),
+            new Vertex(new Vector3f(-0.5f, -0.5f, 0.0f)),
+            new Vertex(new Vector3f( 0.5f, -0.5f, 0.0f)),
+            new Vertex(new Vector3f( 0.5f,  0.5f, 0.0f))
+    }, new int[] {
+            0, 1, 2,
+            0, 3, 2
+    });
+    public Renderer renderer ;
     Window window = new Window(800,800,"DUPA");
+    private void render() {
+        renderer.renderMesh(mesh);
+        window.swapBuffer();
+    }
+public void run(){
+    renderer = new Renderer();
     window.init();
     window.create();
-    window.setBackgorung(0,1,0);
+    window.setBackgorung(0,1,0
+    );
     while (!window.Close()){
         window.update();
+        render();
         window.swapBuffer();
+
         if(Input.isKeyDown(GLFW_KEY_ENTER)){
             System.out.println("tak");
         }
---
+++ src/main/java/org/example/graphics/Vertex.java
@@ -0,0 +1,16 @@
+package org.example.graphics;
+
+
+import org.example.math.Vector3f;
+
+public class Vertex {
+    private Vector3f position;
+
+    public Vertex(Vector3f position) {
+        this.position = position;
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+}
